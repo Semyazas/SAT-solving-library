0849c6bebbded4320c966c6bfa4c91cbdea62849
Martin Kunst <martin.kunst2001@centrum.cz>
2025-03-17 21:34:13 +0100

    feat(reading DIMACS.py)

---
+++ DIMACS_reader.py
@@ -0,0 +1,25 @@
+from Syntax_tree import read_file
+
+def read_DIMACS(filename: str) -> tuple[list[list[int]], list[int]]:
+    clauses = []
+    variables = []
+    lines = read_file(filename)
+
+    for line in lines:
+    
+        if line.startswith('p'):
+            num_vars, num_clauses = int(line.split()[2]),int(line.split()[3])
+            variables = [i+1 for i in range(num_vars)]
+
+        if line.split()[0].lstrip("+-").isnumeric():
+            clause = list(map(int, line.split()[:-1]))
+            clauses.append(clause)
+    return clauses, variables
+
+
+if __name__ == '__main__':
+    print(read_DIMACS("test_file_dimacs.txt"))
+
+
+        
+        
